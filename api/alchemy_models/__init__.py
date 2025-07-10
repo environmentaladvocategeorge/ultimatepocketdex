407a275a9675ec732aa381716143a8a74384f292
Jorge Jimenez <environmentaladvocatejorge@gmail.com>
2025-07-09 21:20:19 -0400

    more fix

--- api/alchemy_models/__init__.py
+++ api/alchemy_models/__init__.py
@@ -1,3 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+from .card_set import CardSet
+from .card_series import CardSeries
+
+__all__ = ["CardSet", "CardSeries"]
